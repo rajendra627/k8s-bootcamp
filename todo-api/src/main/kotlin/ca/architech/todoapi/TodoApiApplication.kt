c733d7733f8ff65fd63673197033f06a9229ec9c
Jungho <jk@architech.ca>
2017-11-20 12:42:34 -0500

    added todo react app and todo java/spring api

---
+++ todo-api/src/main/kotlin/ca/architech/todoapi/TodoApiApplication.kt
@@ -0,0 +1,11 @@
+package ca.architech.todoapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TodoApiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TodoApiApplication::class.java, *args)
+}
