029c1dcebe00291a9020f9188e6de3b1e65eacc4
Jungho <jk@architech.ca>
2017-11-22 16:46:05 -0500

    removed host and changed db name

--- docker/mongo-seed/import_data.sh
+++ docker/mongo-seed/import_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mongoimport --host mongodb --db reach-engine --collection todos --type json --file todos.json --jsonArray
-mongoimport --host mongodb --db reach-engine --collection users --type json --file users.json --jsonArray
+mongoimport --db todo-app --collection todos --type json --file todos.json --jsonArray
+mongoimport --db todo-app --collection users --type json --file users.json --jsonArray
