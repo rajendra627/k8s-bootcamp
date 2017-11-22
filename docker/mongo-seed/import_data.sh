6eaa0c2058c84b780c253d3c2d338ea92c287921
Jungho <jk@architech.ca>
2017-11-22 17:19:58 -0500

    comment added

--- docker/mongo-seed/import_data.sh
+++ docker/mongo-seed/import_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+#by default mongoimport will connect to localhost:27017
 mongoimport --db todo-app --collection todos --type json --file todos.json --jsonArray
 mongoimport --db todo-app --collection users --type json --file users.json --jsonArray
