01b3d95d48787983aa6dc53cbe16cab62cba1dec
Jungho <jk@architech.ca>
2017-11-23 15:12:52 -0500

    fixed test data formatting

--- docker/mongo-seed/import_data.sh
+++ docker/mongo-seed/import_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo 'inside import_data.sh'
 
 #by default mongoimport will connect to localhost:27017
 mongoimport --db todo-app --collection todos --type json --file todos.json --jsonArray
