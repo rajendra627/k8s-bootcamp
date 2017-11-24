a706d707bb20e743ae5517e2b7babeea33aafa36
Jungho <jk@architech.ca>
2017-11-24 11:23:27 -0500

    express based API for ReactUI

---
+++ todo-app/todo-ui/api-src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const passport = require('passport');
+const BearerStrategy = require('passport-azure-ad');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
