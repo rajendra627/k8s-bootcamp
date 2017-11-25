dd0a4d2663e7f60f2e3d43ccdfc676157aabec68
Jungho <jk@architech.ca>
2017-11-24 22:37:43 -0500

    updated test

--- todo-app/todo-api/src/test/kotlin/ca/architech/todo/api/TodoApiApplicationTests.kt
+++ todo-app/todo-api/src/test/kotlin/ca/architech/todo/api/TodoApiApplicationTests.kt
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -24,32 +21,32 @@ class TodoApiApplicationTests {
 	@Test
 	fun addTodoItem() {
 		val now = LocalDate.now();
-		val tomorrow = now.plusDays(1);
+
 		val item = TodoItem(id = "123456789",
                 owner = "123456789",
                 description = "Test Item",
                 priority = Priority.HIGH,
-                dueDate = tomorrow,
+                dueDate = now.plusDays(7),
                 tags = mutableListOf("test"))
 
         val item2 = TodoItem(id = "234567890",
                 owner = "234567890",
                 description = "Read policies by end of week.",
                 priority = Priority.HIGH,
-                dueDate = tomorrow,
+                dueDate = now.plusWeeks(4),
                 tags = mutableListOf("policy, HR"))
 
         val item3 = TodoItem(id= "345678901",
                 owner = "345678901",
                 description = "Finish K8S bootcamp outline.",
                 priority = Priority.HIGH,
-                dueDate = tomorrow,
+                dueDate = now.plusDays(5),
                 tags = mutableListOf("K8S", "training"))
 
         repository.save(mutableListOf(item, item2, item3))
         assert(!repository.findAll().isEmpty())
 
-        var retrievedItem = repository.findById(item.id!!)
+        val retrievedItem = repository.findById(item.id!!)
         assert(retrievedItem?.id!! == item.id!!)
 	}
 
