93ee28f20584e86189c59d5e1aa5ddf06eaf54d5
steven mucci <smucci@architech.ca>
2017-11-29 09:48:22 -0500

    added tests for todos reducer

---
+++ todo-app/todo-ui/src/reducers/tests/todos.spec.js
@@ -0,0 +1,59 @@
+import todos from '../todos';
+import actionTypes from '../../actions/actionTypes';
+describe('Todos Reducer', ()=>{
+  it('Should handle undefined initial state', () => {
+    const finalState = [];
+    const initialState = undefined;
+    const action = {
+      type: 'INIT',
+    };
+    const actualState = todos(initialState, action);
+
+    expect(actualState).toEqual(finalState);
+  });
+
+  it('Should handle adding a todo', () => {
+    const initialState = undefined;
+    const todo = {
+      id: 1,
+      description: 'todo',
+      priority: 'HIGH',
+      dueDate: '2017-12-10',
+      completed: false
+    };
+    const finalState = [todo];
+    const action ={
+      type: actionTypes.ADD_TODO,
+      ...todo
+    };
+    const actualState = todos(initialState, action);
+
+    expect(actualState).toEqual(finalState);
+  });
+
+  it('Should handle toggling the status/completion of a todo', () => {
+    const initalTodo = {
+      id: 1,
+      description: 'todo',
+      priority: 'HIGH',
+      dueDate: '2017-12-10',
+      completed: false
+    };
+    const completedTodo = {
+      id: 1,
+      description: 'todo',
+      priority: 'HIGH',
+      dueDate: '2017-12-10',
+      completed: true
+    };
+    const initialState = [initalTodo];
+    const finalState = [completedTodo];
+    const action ={
+      type: actionTypes.TOGGLE_TODO,
+      id: 1
+    };
+    const actualState = todos(initialState, action);
+
+    expect(actualState).toEqual(finalState);
+  });
+});
