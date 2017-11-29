e89ca4d809acd961d2b2afb5f300ceede4934928
Jungho <jk@architech.ca>
2017-11-29 10:28:28 -0500

    added db initialization

--- todo-app/import_data.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo 'inside import_data.sh'
-
-#by default mongoimport will connect to localhost:27017
-mongoimport --db todo-app --collection todos --type json --file todos.json --jsonArray
-mongoimport --db todo-app --collection users --type json --file users.json --jsonArray
