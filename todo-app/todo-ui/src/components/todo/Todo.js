ebea77adf6f96f122beeb357bcb04837aeda02c9
steven mucci <smucci@architech.ca>
2017-12-06 15:40:09 -0500

    style changes

--- todo-app/todo-ui/src/components/todo/Todo.js
+++ todo-app/todo-ui/src/components/todo/Todo.js
@@ -4,28 +4,33 @@ import './todo.css';
 import TagLabel from '../TagLabel';
 
 const Todo = ({ onClick, onDelete, done, description, priority, dueDate, tags }) => (
-  <div className="col-xs-12">
-    <div className={"col-sm-11 col-xs-10 todo-item " + (done ? 'done' : 'not-done') } onClick={onClick}>
-      <div className="col-xs-5">
+  <div className="col-xs-12 todo-container">
+    <div className={"col-sm-12 col-xs-10 todo-item " + (done ? 'done' : 'not-done') } onClick={onClick}>
+      <div className="col-xs-7 col-sm-5">
         <i className={"fa " + (done ? 'fa-check-square-o fa-lg' : 'fa-square-o fa-lg')}/>
         <span className="description">
       {description}
     </span>
       </div>
-      <div className="col-xs-4 text-right">
+      <div className="col-xs-5 col-sm-4 tag-container">
         {
           tags.length ?
             tags.map(tag =>  <TagLabel key={tags.indexOf(tag)} tag={tag}/>)
             : ''
         }
       </div>
-      <div className="col-xs-3 text-right">
+      <div className="col-xs-12 col-sm-3 date-container">
         <span>{dueDate}</span>
       </div>
     </div>
-    <div className="col-sm-1 col-xs-2 text-right todo-action">
+    <div className="col-xs-2 col-sm-1 hidden-lg text-right todo-action">
       <button className="btn btn-default todo-delete" type="button" onClick={onDelete}>
-        <i class="fa fa-trash-o" aria-hidden="true"/>
+        <i className="fa fa-trash-o" aria-hidden="true"/>
+      </button>
+    </div>
+    <div className="todo-action hidden-md hidden-sm hidden-xs">
+      <button className="btn btn-default todo-delete" type="button" onClick={onDelete}>
+        <i className="fa fa-trash-o" aria-hidden="true"/>
       </button>
     </div>
   </div>
--- todo-app/todo-ui/src/components/todoList/TodoList.js
+++ todo-app/todo-ui/src/components/todoList/TodoList.js
@@ -5,39 +5,34 @@ import './todoList.css';
 
 const TodoList = ({ upcomingTodos, pastDueTodos, todayTodos, onTodoClick, onDelete }) => (
   <div className="row">
-    <div className="col-sm-12">
-      <h1 href="#today-todo" data-toggle="collapse">Today <small>({todayTodos.length})</small></h1>
-      <div id="today-todo" className="collapse in">
+    <div className="col-xs-12">
+      <h1>Today <small>({todayTodos.length})</small></h1>
           {
             todayTodos
               .map(todo =>
                 (<Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo)} onDelete={() => onDelete(todo.id)}/>)
               )
           }
-      </div>
     </div>
-    <div className="col-sm-12">
-      <h1 href="#past-due" data-toggle="collapse">Past Due <small>({pastDueTodos.length})</small></h1>
-      <div id="past-due" className="collapse in">
+    <div className="col-xs-12">
+      <h1>Past Due <small>({pastDueTodos.length})</small></h1>
+
         {
           pastDueTodos
             .map(todo =>
               (<Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo)} onDelete={() => onDelete(todo.id)}/>)
             )
         }
-      </div>
     </div>
 
-    <div className="col-sm-12">
-      <h1 href="#upcoming" data-toggle="collapse">Upcoming <small>({upcomingTodos.length})</small></h1>
-      <div id="upcoming" className="collapse in">
+    <div className="col-xs-12">
+      <h1 >Upcoming <small>({upcomingTodos.length})</small></h1>
         {
           upcomingTodos
             .map(todo =>
               (<Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo)} onDelete={() => onDelete(todo.id)}/>)
             )
         }
-      </div>
     </div>
   </div>
 );
