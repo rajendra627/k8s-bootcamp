f30268cba4f2b2fec1f37c7e8d5b1a1f35d7b339
Jungho <jk@architech.ca>
2017-12-06 08:15:59 -0500

    kotlin version of todo-api

---
+++ todo-app/todo-api-kotlin/src/main/kotlin/ca/architech/todo/api/TodoItemRepository.kt
@@ -0,0 +1,20 @@
+package ca.architech.todo.api
+
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
+
+interface TodoItemRepository : MongoRepository<TodoItem, String> {
+    fun findById(id: String) : TodoItem?
+    fun findByPriority(priority: Priority) : List<TodoItem>?
+    fun findAllByOrderByDueDateDesc() : List<TodoItem>?
+
+    @Query(value="{ 'tags' : ?0 }")
+    fun findByTag(tag: String) : List<TodoItem>?
+
+    @Query(value="{ 'done': true }")
+    fun findDone() : List<TodoItem>?
+
+    @Query(value="{ 'done': false }")
+    fun findNotDone() : List<TodoItem>?
+
+}
