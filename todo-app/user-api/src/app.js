423b8108e6134fcca723a6880f3100c3f24daca7
Oliver Sy <ocs@architech.ca>
2017-12-07 09:59:25 -0500

    Add unit tests

---
+++ todo-app/user-api/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const logger = require('./logger');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api/user', require('./controller'));
+
+module.exports = app;
