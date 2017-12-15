2f5b82986835b071c806948fac7d23e61aaf6b58
steven mucci <smucci@architech.ca>
2017-12-15 15:29:59 -0500

    added loading and error states

--- todo-app/todo-ui/src/reducers/index.js
+++ todo-app/todo-ui/src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
 import searchFilter from './searchFilter';
+import loading from './loading';
+import error from './errors';
 
 const todoApp = combineReducers({
   todos,
   visibilityFilter,
-  searchFilter
+  searchFilter,
+  loading,
+  error
 });
 
 export default todoApp;
