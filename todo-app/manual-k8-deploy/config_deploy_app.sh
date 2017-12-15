d9fca2e030474e6fa29b95289296fb8bca79506a
Ahmed-Khalifa-Architech <akhalifa@architech.ca>
2017-12-15 14:24:07 -0500

    my work

--- todo-app/manual-k8-deploy/config_deploy_app.sh
+++ todo-app/manual-k8-deploy/config_deploy_app.sh
@@ -3,4 +3,4 @@ todoapp_namespace="todoapp"
 kubectl create namespace $todoapp_namespace
 kubectl create secret generic todoapp-secrets --from-file=secrets --namespace=$todoapp_namespace
 kubectl create --namespace=$todoapp_namespace -f todo-config.yaml
-kubectl create --namespace=$todoapp_namespace -f todo-app.yamlc
+kubectl create --namespace=$todoapp_namespace -f todo-app.yaml
