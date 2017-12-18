5c2e51bb751303082d7a6a843350fbaaa4ff3d19
Oliver Sy <ocs@architech.ca>
2017-12-18 11:44:39 -0500

    Update repository methods

--- todo-app/todo-api-java/src/main/java/ca/architech/todo/TodoItemRepository.java
+++ todo-app/todo-api-java/src/main/java/ca/architech/todo/TodoItemRepository.java
@@ -8,18 +8,20 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface TodoItemRepository extends MongoRepository<TodoItem, String> {
-    TodoItem findById(String id);
+    TodoItem findByIdAndOwner(String id, String owner);
 
-    List<TodoItem> findByPriority(Priority priority);
+    List<TodoItem> findByOwner(String owner);
 
-    List<TodoItem> findAllByOrderByDueDateDesc();
+    List<TodoItem> findByPriorityAndOwner(Priority priority, String owner);
 
-    @Query(value = "{ 'tags' : ?0 }")
-    List<TodoItem> findByTag(String tag);
+    List<TodoItem> findAllByOwnerOrderByDueDateDesc(String owner);
 
-    @Query(value = "{ 'done': true }")
-    List<TodoItem> findDone();
+    @Query(value = "{ 'tags' : ?0, 'owner' : ?1 }")
+    List<TodoItem> findByTagAndOwner(String tag, String owner);
 
-    @Query(value = "{ 'done': false }")
-    List<TodoItem> findNotDone();
+    @Query(value = "{ 'done': true, 'owner': ?1 }")
+    List<TodoItem> findDoneByOwner(String owner);
+
+    @Query(value = "{ 'done': false, 'owner': ?1 }")
+    List<TodoItem> findNotDoneByOwner(String owner);
 }
