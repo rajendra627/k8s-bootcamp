1be24bc55bd60fe22b1a2b98b929e2175ab40d57
Paul Mooney <pmooney@architech.ca>
2018-01-03 10:44:54 -0500

    New End-2-End test container for todo-ui.
    Runs end-2-end tests inside of a headless chrome browser against the todo-ui.

--- todo-app/todo-ui/src/test/e2e/basic.e2e.js
+++ todo-app/todo-ui/src/test/e2e/basic.e2e.js
@@ -1,6 +1,9 @@
+const host = process.env.APPLICATION_BASE_URL || "http://localhost:3000";
+
+
 describe('TODO Demo App', function() {
   it('should have a title', function() {
-    browser.get('http://localhost:3000/');
+    browser.get(`${host}/`);
 
     expect(browser.getTitle()).toEqual('Todo React App');
   });
