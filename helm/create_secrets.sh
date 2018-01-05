a41c4a47b3fafdda8d1a7973299b6311324cdfb3
Igor Ljaskevic <iljaskevic@architech.ca>
2018-01-05 15:03:36 -0500

    Updated provisioning scripts

--- helm/create_secrets.sh
+++ helm/create_secrets.sh
@@ -2,10 +2,10 @@
 if [ "$#" -ne 1 ] ; then
   echo "Usage: $0 namespace" >&2
   echo "assuming it is default"
-  kubectl create secret generic todoapp-secrets --from-file=secrets 
+  kubectl create secret generic todoapp-secrets --from-file=secrets
  else
   todoapp_namespace=$1
-  kubectl create namespace $todoapp_namespace
-  kubectl create secret generic todoapp-secrets --from-file=secrets --namespace=$todoapp_namespace
+  kubectl --kubeconfig ~/.kube/acsGenerated.config create namespace $todoapp_namespace
+  kubectl --kubeconfig ~/.kube/acsGenerated.config create secret generic todoapp-secrets --from-file=secrets --namespace=$todoapp_namespace
 fi
 
