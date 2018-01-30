a95493b11d18894591bbab21e3ad5557c6ba9573
Jungho <jk@architech.ca>
2018-01-30 15:48:29 -0500

    removed duplication for env vars, create namespace

--- helm/create_secrets.sh
+++ helm/create_secrets.sh
@@ -2,6 +2,7 @@
 if [ "$#" -ne 1 ] ; then
   echo "Usage: $0 namespace" >&2
   echo "assuming it is todo-app"
+  kubectl create namespace todo-app
   kubectl create secret generic todoapp-secrets --from-file=secrets --namespace=todo-app
  else
   todoapp_namespace=$1
