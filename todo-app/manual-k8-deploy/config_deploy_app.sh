a6f29b8e7cb94123e86b62953c189a9d2c0b0af9
Jungho <jk@architech.ca>
2018-01-30 08:31:00 -0500

    refactoring

--- todo-app/manual-k8-deploy/config_deploy_app.sh
+++ todo-app/manual-k8-deploy/config_deploy_app.sh
@@ -2,5 +2,6 @@
 todoapp_namespace="todo-app"
 kubectl create namespace $todoapp_namespace
 kubectl create secret generic todoapp-secrets --from-file=secrets --namespace=$todoapp_namespace
-kubectl create --namespace=$todoapp_namespace -f todo-config.yaml
+kubectl create --namespace=$todoapp_namespace -f todo-api-config.yaml
+kubectl create --namespace=$todoapp_namesapce -f user-api-config.yaml
 kubectl create --namespace=$todoapp_namespace -f todo-app.yaml
