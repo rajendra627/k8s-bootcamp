3181aa189457bd383e8d98db933640b73cb75bec
Jungho <jk@architech.ca>
2018-02-06 11:02:25 -0500

    added ingress cluster role/rolebindings to support clusters with RBAC

--- ingress/create-ingress-example.sh
+++ ingress/create-ingress-example.sh
@@ -3,6 +3,10 @@
 namespace=ingress-example
 
 kubectl create namespace $namespace
+#If you have RBAC enabled on your K8S cluster
+#You MUST uncomment the first 2 lines
+#kubectl create -f ingress-cluster-role.yml
+#kubectl create -f ingress-role-binding.yml --namespace $namespace
 kubectl create -f ingress-controller.yml --namespace $namespace
 kubectl create -f pod-details-service-v1.yml --namespace $namespace
 kubectl create -f pod-details-service-v2.yml --namespace $namespace
